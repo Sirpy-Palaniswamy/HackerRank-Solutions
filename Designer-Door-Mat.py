689eb9af8ecaaacde6e1af843038923d1077edf4
Sirpy <50924555+Sirpy-Palaniswamy@users.noreply.github.com>
2020-07-27 17:40:51 +0530

    Create Designer-Door-Mat.py

---
+++ Designer-Door-Mat.py
@@ -0,0 +1,24 @@
+#The Prolem:
+
+"""
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+Mat size must be X. ( is an odd natural number, and  is  times .)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+Sample Designs
+
+    Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+"""
+
+#The Code:
+
+n, m = map(int,input().split())
+pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
+print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
