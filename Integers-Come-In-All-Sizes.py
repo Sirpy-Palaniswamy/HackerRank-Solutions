6d8d466d34cbf37f596b36a5597279c8e8d8f0d6
Sirpy <50924555+Sirpy-Palaniswamy@users.noreply.github.com>
2020-08-05 21:24:20 +0530

    Create Integers-Come-In-All-Sizes.py

---
+++ Integers-Come-In-All-Sizes.py
@@ -0,0 +1,11 @@
+#Problem
+"""
+Read four numbers, a, b, c, and d, and print the result of a^b + c^d.
+"""
+
+#Code
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print((a**b)+(c**d))
