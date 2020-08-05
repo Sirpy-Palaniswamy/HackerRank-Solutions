82d554936da5bbf090a5697f9531fdf436870eea
Sirpy <50924555+Sirpy-Palaniswamy@users.noreply.github.com>
2020-08-05 21:17:07 +0530

    Create Power-Mod_Power.py

---
+++ Power-Mod_Power.py
@@ -0,0 +1,14 @@
+#Problem
+"""
+You are given three integers: a, b, and m, respectively. 
+Print two lines.
+The first line should print the result of pow(a,b). 
+The second line should print the result of pow(a,b,m).
+"""
+
+#Code
+a = int(input())
+b = int(input())
+m = int(input())
+print(pow(a,b))
+print(pow(a,b,m))
