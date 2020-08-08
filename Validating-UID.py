bf8366dd7d3436d0636fc5d95a65976152d2795e
Sirpy <50924555+Sirpy-Palaniswamy@users.noreply.github.com>
2020-08-08 23:28:11 +0530

    Create Validating-UID.py
    To have a more clear view on how regex works visit : 
    1. https://docs.python.org/3/howto/regex.html
    2. https://www.w3schools.com/python/python_regex.asp (For more basic understanding of the topic)

---
+++ Validating-UID.py
@@ -0,0 +1,30 @@
+#Problem
+"""
+ABCXYZ company has up to 1000 employees.
+The company decides to create a unique identification number (UID) for each of its employees.
+The company has assigned you the task of validating all the randomly generated UIDs.
+
+A valid UID must follow the rules below:
+
+It must contain at least 2 uppercase English alphabet characters.
+It must contain at least 3 digits (0 - 9).
+It should only contain alphanumeric characters (a - z, A - Z & 0 - 9).
+No character should repeat.
+There must be exactly 10 characters in a valid UID.
+"""
+
+#Code
+import re
+
+for i in range(int(input())):
+    UID = ''.join(sorted(input()))
+    try:
+        assert re.search(r'[A-Z]{2}', UID)
+        assert re.search(r'\d\d\d', UID)
+        assert not re.search(r'[^a-zA-Z0-9]', UID)
+        assert not re.search(r'(.)\1', UID)
+        assert len(UID) == 10
+    except:
+        print("Invalid")
+    else:
+        print("Valid")
