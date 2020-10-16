25b028112a65fefff3ec6d9f1e0a29e72f9b92b4
Sirpy <50924555+Sirpy-Palaniswamy@users.noreply.github.com>
2020-10-16 19:29:07 +0530

    Create Default-Arguments.py

---
+++ Default-Arguments.py
@@ -0,0 +1,39 @@
+#Problem
+"""
+In this challenge, the task is to debug the existing code to successfully execute all provided test files.
+"""
+
+#Code
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+#Main Change
+def print_from_stream(n, stream=EvenStream()):
+    stream.__init__()
+    for _ in range(n):
+        print(stream.get_next())
+
+
+queries = int(input())
+for _ in range(queries):
+    stream_name, n = input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
